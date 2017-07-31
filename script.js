eee698fd53e1d4bde6ca91ad68db1bcf9a8da2b3
Stephan Exius <sexius@westinghousehs.org>
2017-07-31 15:43:47 +0000

    commit messages

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // JavaScript File
 // JavaScript File
+
 $(document).ready(function(){
-$( "#founderButton" ).click(function() {
+$( "#founderButton" ).click(function() { 
     var founderInput = $("#founder").val();
-    if (founderInput === "Steve Jobs" || "Ronald Wayne" || "Steve Wozniak") {
+    if (founderInput === "Steve Jobs") {
         alert("You are correct");
     }else{
         alert("Oops, Try again");
